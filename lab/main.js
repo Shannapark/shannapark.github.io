9c6f01605bdffa8f1bdb2a6161f5590c37df1eca
Shannapark <47001567+Shannapark@users.noreply.github.com>
2019-04-04 15:46:44 -0400

    Add files via upload

---
+++ lab/main.js
@@ -0,0 +1,60 @@
+alert("Welcome! Please view console on your broswer");
+
+
+var Name = "shanna park";
+console.log(Name);
+
+var Age = 19;
+console.log(Age);
+
+var Birthday = "06/04/1999";
+console.log(Birthday);
+
+var admin = false;
+console.log(admin);
+
+var myself = Name + Age;
+console.log(myself);
+
+
+console.log(29 - 10);
+console.log(10 + 9);
+console.log(13 + 6);
+console.log(19 * 0);
+console.log(19 / 1);
+
+
+
+var countries = ["italy", "germany", "hong kong", "seoul"]
+console.log(countries[0]);
+console.log(countries.length);
+
+
+var currentTime = "The current time is ";
+var time =  new Date().getHours() + ":" + new Date().getMinutes() + ".";
+
+console.log(currentTime + time);
+
+
+var currentHour = new Date().getHours();
+
+if (currentHour == 12) {
+  console.log("It is 12 now.")
+} else {
+  console.log("It is not 12 yet.")
+};
+
+
+console.log (myName.length);
+myName = myName.length - 1;
+
+if (myName <= 9) {
+  console.log("My name is relatively short.")
+} else if (myName >= 14) {
+  console.log("My name is relatively long.")
+} else {
+  console.log("My name is neither short nor long.")
+};
+
+
+
