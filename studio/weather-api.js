6ef488282c5c9644aa42c9464ae8720b50888a76
Shannapark <47001567+Shannapark@users.noreply.github.com>
2019-04-25 11:05:48 -0400

    Add files via upload

---
+++ studio/weather-api.js
@@ -0,0 +1,42 @@
+var humidity;
+
+
+$(document).ready(function () {
+
+
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?id=5128581&units=metric&APPID=f672f030db4cd256883d4ea0f4080826', 
+        dataType: 'jsonp',
+        success: function (results) {
+            humidity = results.main.humidity;
+            myWeatherInterpretation();
+}
+});
+    
+    function myWeatherInterpretation() {
+       $('p').append('humidity: ' + humidity);
+}    
+});
+
+
+//part2
+$(document).ready(function () {
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?id=4180439&units=metric&APPID=f672f030db4cd256883d4ea0f4080826', 
+        dataType: 'jsonp',
+        success: function (results) {
+
+            humidity2 = results.main.humidity;
+        
+
+            myWeatherInterpretation();
+}
+});
+    
+
+
+    function myWeatherInterpretation() {
+        
+    $('p2').append('humidity: ' + humidity2);
+}
+});
