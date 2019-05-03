a530056cf69481bafc1a3f00d253e72575588f0e
Shannapark <47001567+Shannapark@users.noreply.github.com>
2019-05-03 12:14:25 -0400

    Add files via upload

--- studio/final-api.js
+++ studio/final-api.js
@@ -1,14 +1,16 @@
 
 var humidity1;
+var humidity2;
 
 
 //Atlanta
 $(document).ready(function () {
 
     $.ajax({
+
         url: 'https://api.openweathermap.org/data/2.5/weather?id=4180439&units=metric&APPID=f672f030db4cd256883d4ea0f4080826',
         dataType: 'jsonp',
-        success: function(results) {
+        success: function (results) {
         
             humidity1 = results.main.humidity;
 
@@ -19,21 +21,50 @@ $(document).ready(function () {
     });
 
     function myWeatherInterpretation() {
-        $('.atl').append('</br> '  + humidity1 );
-
+        $('#one').append('</br> '  + humidity1);
+            
+                
+        $(".bottle1").css({"animation-duration": "1s",});
+        $(".cap1").css({"animation-duration": "1s",});
+        
+        console.log(humidity1);
+        
+            if (humidity1 < 50) {
+      $('.bottle1').css({
+        "animation-duration": "6s"
+      });
+    }
+            if (humidity1 < 50) {
+      $('.cap1').css({
+        "animation-duration": "6s"
+      });
+    }
+              else if (humidity1 > 50) {
+      $('.bottle1').css({
+        "animation-duration": "1s"
+      });
+    }
+         else if (humidity1 > 50) {
+      $('.cap1').css({
+        "animation-duration": "1s"
+      });
+    }
+       
+        
     }
 });
+        
+        
 
 
 
+/////////////New York////////////
+$(document).ready(function () {
 
-//New York
-$(document).ready(function() {
-
-    $.ajax({
+$.ajax({
         url: 'https://api.openweathermap.org/data/2.5/weather?id=5128581&units=metric&APPID=f672f030db4cd256883d4ea0f4080826',
         dataType: 'jsonp',
-        success: function(results){
+        success: function (results) {
       
             humidity2 = results.main.humidity;
 
@@ -44,8 +75,31 @@ $(document).ready(function() {
     });
 
     function myWeatherInterpretation() {
-        $('.newyork').append('</br> '+ humidity2 );
+        $('#two').append('</br> ' + humidity2);
+        
+        $(".bottle2").css({"animation-duration": "1s",});
+        $(".cap2").css({"animation-duration": "1s",});
+        
+        console.log(humidity2);
+        
+            if (humidity2 = 50) {
+      $('.bottle2').css({
+        "animation-duration": "1s"
+      });
+    }
+            if (humidity2 = 50) {
+      $('.cap2').css({
+        "animation-duration": "1s"
+      });
+    }
 
+        
+        
+        
+        
+        
+        
+        
     }
 });
 
