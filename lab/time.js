11947a55ae0ce37c9779916d2a2b068e472a5a22
Shannapark <47001567+Shannapark@users.noreply.github.com>
2019-05-05 18:17:50 -0400

    Add files via upload

---
+++ lab/time.js
@@ -0,0 +1,272 @@
+	function showTime(){ 
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds();
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+        
+    document.getElementById("ClockDisplay").innerText = time;
+    document.getElementById("ClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);}
+
+showTime();
+
+
+
+  var s = setTimeout(showTime, 500);
+    if (s == 0) {
+      $('.color').addClass('.white');
+    }
+    
+   if (s == 1) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 2) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 3) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 4) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 5) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 6) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 7) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 8) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 9) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 10) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 11) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 12) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 13) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 14) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 15) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 16) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 17) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 18) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 19) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 20) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 21) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 22) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 23) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 24) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 25) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 26) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 27) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 28) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 29) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 30) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 31) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 32) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 33) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 34) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 35) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 36) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 37) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 38) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 39) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 40) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 41) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 42) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 43) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 44) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 45) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 46) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 47) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 48) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 49) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 50) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 51) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 52) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 53) {
+      $('.color').addClass('.pink');
+    }
+
+    if (s == 54) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 55) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 56) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 57) {
+      $('.color').addClass('.pink')
+    }
+
+    if (s == 58) {
+      $('.color').addClass('.white');
+    }
+
+    if (s == 59) {
+      $('.color').addClass('.pink');
+    }
+
